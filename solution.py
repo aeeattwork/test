b9e3ed6896a7c9a1111fe2c73c6b59d7963eb1f3
aeeattwork <42655582+aeeattwork@users.noreply.github.com>
2018-08-23 21:56:20 +0200

    v 0.1.0
    - Adding function to read text files
    - splitting text by white spaces and removing punctuations

--- solution.py
+++ solution.py
@@ -3,3 +3,13 @@ import os
 import re
 import sys
 import string
+
+def read_file(file):
+    words = []
+    with open(file,'r') as f:
+        for line in f:
+            #for word in re.split(r"--\s|\n",line):
+            for word in line.split():
+                word = word.strip(string.punctuation)
+                words.append(word)
+    return words
