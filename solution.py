868ddc655fbf41cd1039eef65bd90c96616f0edd
aeeattwork <42655582+aeeattwork@users.noreply.github.com>
2018-08-23 22:00:25 +0200

    v 0.2.0
    1) Word frequency (list of words with its number of occurrences). The program should output only the 10 most frequent words.
    - Adding function to define 10 most frequent words and their number of occurrence
    - Additionally, adding variable to define number of occurrence of every word

--- solution.py
+++ solution.py
@@ -13,3 +13,9 @@ def read_file(file):
                 word = word.strip(string.punctuation)
                 words.append(word)
     return words
+
+def occurrence(file):
+    output = read_file(file)
+    number_of_occurrence = Counter(output)
+    most_popular = Counter(output).most_common(10)
+    return (number_of_occurrence, most_popular)
