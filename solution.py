ed94f2d5575963b2028c72d85dc35eb421c5db2a
aeeattwork <42655582+aeeattwork@users.noreply.github.com>
2018-08-23 23:58:29 +0200

    v 0.3.0
    2) All words present in all three files. (Words that can be found on all 3 documents) 
    3) All words present in a file but not on the other two files, per file. (Words that can be found only on one document)
    - Adding main function:
     -- Providing input files. Input could be provided parametrically (in comment).
     -- Reading input and evaluating by:
      --- intersection of the 3 lists
      --- unique values of each list
     -- Printing results:
      --- The 10 most popular words in each file
      --- Words that can be found in all files
      --- Words that can only be found in each file

--- solution.py
+++ solution.py
@@ -19,3 +19,31 @@ def occurrence(file):
     number_of_occurrence = Counter(output)
     most_popular = Counter(output).most_common(10)
     return (number_of_occurrence, most_popular)
+
+def main():
+    #file1 = input('Enter file_1 root: ')
+    #file2 = input('Enter file_2 root: ')
+    #file3 = input('Enter file_3 root: ')
+    file1 = "D:\Python\TEST\\avid_python_test\\file_1.txt"
+    file2 = "D:\Python\TEST\\avid_python_test\\file_2.txt"
+    file3 = "D:\Python\TEST\\avid_python_test\\file_3.txt"
+    oc1, pop1 = occurrence(file1)
+    oc2, pop2 = occurrence(file2)
+    oc3, pop3 = occurrence(file3)
+    files = []
+    f1 = read_file(file1)
+    f2 = read_file(file2)
+    f3 = read_file(file3)
+    matches = set(f1).intersection(f2, f3)
+    unique1 = set(f1).difference(f2,f3)
+    unique2 = set(f2).difference(f1,f3)
+    unique3 = set(f3).difference(f1,f2)
+    print('\n',"The 10 most popular words in file1:",'\n','\n',pop1)
+    print('\n',"The 10 most popular words in file2:",'\n','\n',pop2)
+    print('\n',"The 10 most popular words in file3:",'\n','\n',pop3)
+    print('\n',"Words that can be found in all files:", '\n','\n',matches)
+    print('\n',"Words that can only be found in file1:", '\n','\n',unique1)
+    print('\n',"Words that can only be found in file2:", '\n','\n',unique2)
+    print('\n',"Words that can only be found in file3:", '\n','\n',unique3)
+
+main()
