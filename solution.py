ce4164f22e25add4b83be311742e320056f4822a
aeeattwork <42655582+aeeattwork@users.noreply.github.com>
2018-08-26 16:57:09 +0200

    v 0.5.0
    - Generalizing occurrence function

--- solution.py
+++ solution.py
@@ -29,10 +29,9 @@ def read_docx(file):
     words = list(filter(None, words))
     return words
 
-def occurrence(file):
-    output = read_file(file)
-    number_of_occurrence = Counter(output)
-    most_popular = Counter(output).most_common(10)
+def occurrence(read):
+    number_of_occurrence = Counter(read)
+    most_popular = Counter(read).most_common(10)
     return (number_of_occurrence, most_popular)
 
 def main():
